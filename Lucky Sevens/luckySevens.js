8fb33eeaf7ae60d3d7c70f25e0d946c62d714834
Tou Her <touyangher@gmail.com>
2018-03-12 22:13:35 -0500

    restaurant part 3 added

--- Lucky Sevens/luckySevens.js
+++ Lucky Sevens/luckySevens.js
@@ -1,39 +1,44 @@
 function playGame () {
-  var startBet = document.getElementById("startingAmount").value; //parseInt if not working
+  //get bet
+  var startBet = document.getElementById("startingAmount").value;
 
-  if (startBet <=0) {
+
+  //display error if bet less than 0
+    if(startBet <= 0) {
     alert("Please bet more than zero dollars!");
-    document.getElementById("result").style.display = "none";       //good
+    document.getElementById("result").style.display = "none";
   } else {
-    document.getElementById("result").style.display = "block";    //good
+    //display results table
+    document.getElementById("result").style.display = "block";
   }
-
   var earn = 4;
   var lose = 1;
-  var total = startBet;
+  var gameMoney = startBet;
   var maxMoney = startBet;
   var rollCounter = 0;
-  var maxRoll =0;
-
-  while(total > 0){
+  var maxRoll = 0;
+    //loop to play game only if there is gameMoney
+  while(gameMoney > 0){
+    //roll dice, max = 12, min = 2
     var roleDice = Math.ceil(Math.random() * (1 + 12 - 2));
+    rollCounter++
+    //add to gameMoney if dice roll is 7
     if (roleDice == 7){
-      total = total + earn;
-      if(total > maxMoney){
-     		 max = total;
+      gameMoney = gameMoney + earn;
+      //take info the max amount and roll count for max amout
+      if(gameMoney > maxMoney){
+     		 maxMoney = gameMoney;
          maxRoll = rollCounter;
      	 }
+       //lose money if dice roll is not 7
     } else {
-      total = total - lose;
+      gameMoney = gameMoney - lose;
     }
-    rollCounter++
   }
-
 
   document.getElementById("betAmount").innerText= "$ "+ startBet + ".00";
   document.getElementById("brokeRolls").innerText= rollCounter;
   document.getElementById("highWin").innerText= "$ " + maxMoney +".00";
   document.getElementById("highWinRoll").innerText= maxRoll;
   document.getElementById("buttonClick").innerText = "Play Again"; //change the button text to play again
-
 }
